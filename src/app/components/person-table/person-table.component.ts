d3057375c0f2db64655a56a4b59df81da1ded6f3
Raf. Mavragani <efimavragani99@gmail.com>
2025-04-28 18:08:09 +0300

    new fixes

--- src/app/components/person-table/person-table.component.ts
+++ src/app/components/person-table/person-table.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Person } from 'src/app/shared/interfaces/person';
 
 @Component({
   selector: 'app-person-table',
@@ -7,12 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './person-table.component.css'
 })
 export class PersonTableComponent {
-  name = "Thanasis"
+  @Input() personInput : Person | undefined;
+  name = "Siba"
 
   person = {
-    givenName:"Thanasis",
-    surName: "Androutsos",
-    age: 20,
+    givenName:"Siba",
+    surName: "Mav",
+    age: 10,
     email:'thanasis@aueb.gr'
   }
 }
